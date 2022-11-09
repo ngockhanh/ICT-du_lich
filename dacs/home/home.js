8916ee4add50937c14457d0ed46f4374fb8fdeb7
phandat00011469 <20010943@st.phenikaa-uni.edu.vn>
2022-11-09 14:28:24 +0700

    lan11

--- dacs/home/home.js
+++ dacs/home/home.js
@@ -1,8 +1,15 @@
 function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
-    mapId: "8e0a97af9386fef",
-    center: { lat: 48.85, lng: 2.35 },
-    zoom: 12,
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
   });
 }
 
