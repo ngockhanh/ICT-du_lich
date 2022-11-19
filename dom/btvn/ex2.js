02bdffc1c5c0a334d0a6d7be2f0a38f759439113
phandat00011469 <20010943@st.phenikaa-uni.edu.vn>
2022-11-19 08:43:19 +0700

    18

--- dom/btvn/ex2.js
+++ dom/btvn/ex2.js
@@ -1,4 +1,11 @@
 var soLieu_4, soLieu_5, soLieu_6;
 function kiem_tra(soLieu_1, soLieu_2, soLieu_3){
-
+  soLieu_4 = Math.round((soLieu_1/30/12) + (soLieu_1*(soLieu_2/100))/12);// so tien phai tra hang thang
+  soLieu_5 = soLieu_4 *12*30;//tong so tien phai tra
+  soLieu_6 = Math.round(soLieu_1 * (soLieu_2/100) *30);// tong so lai
+document.querySelector('#soLieu4').value= soLieu_4;
+document.querySelector('#soLieu5').value= soLieu_5;
+document.querySelector('#soLieu6').value= soLieu_6;
 }
+a = document.querySelector('#solieu4')
+console.log([a])
