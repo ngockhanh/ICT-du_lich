4ea72f4438689e31a516f85f381941b96c128d07
Phan-Thanh-DaT-2002 <phandat2002h@gmail.com>
2022-11-20 22:29:33 +0700

    19

--- dom/btvn/ex1.js
+++ dom/btvn/ex1.js
@@ -19,18 +19,28 @@ function ket_qua(a) {
 function kiem_tra(ten, chieuCao, canNang, gioiTinh) {
   var bmi = canNang / (chieuCao ** 2);
   if (gioiTinh === 'nam') {
-    if (bmi < 20.7) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(1) + ' với điểm bmi là: ' + bmi;
-    else if (20.7 < bmi && bmi < 26.4) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(2)+ ' với điểm bmi là: ' + bmi;
-    else if (26.4 < bmi && bmi < 27.8) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(3)+ ' với điểm bmi là: ' + bmi;
-    else if (27.8 < bmi && bmi < 31.1) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(4)+ ' với điểm bmi là: ' + bmi;
-    else if (31.1 < bmi) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(5)+ ' với điểm bmi là: ' + bmi;
+    if (bmi < 20.7)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(1) + ' với điểm bmi là: ' + bmi;
+    else if (20.7 < bmi && bmi < 26.4)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(2) + ' với điểm bmi là: ' + bmi;
+    else if (26.4 < bmi && bmi < 27.8)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(3) + ' với điểm bmi là: ' + bmi;
+    else if (27.8 < bmi && bmi < 31.1)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(4) + ' với điểm bmi là: ' + bmi;
+    else if (31.1 < bmi)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(5) + ' với điểm bmi là: ' + bmi;
   }
-  else  if (gioiTinh === 'nu'){
-    if (bmi < 19.1) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(1)+ ' với điểm bmi là: ' + bmi;
-    else if (19.1 < bmi && bmi < 25.8) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(2)+ ' với điểm bmi là: ' + bmi;
-    else if (25.8 < bmi && bmi < 27.3) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(3)+ ' với điểm bmi là: ' + bmi;
-    else if (27.3 < bmi && bmi < 32.3) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(4)+ ' với điểm bmi là: ' + bmi;
-    else if (32.3 < bmi) return document.querySelector('.ket_qua').innerHTML='Bạn: ' + ten +" có tình trạng "+ ket_qua(5)+ ' với điểm bmi là: ' + bmi;
+  else if (gioiTinh === 'nu') {
+    if (bmi < 19.1)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(1) + ' với điểm bmi là: ' + bmi;
+    else if (19.1 < bmi && bmi < 25.8)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(2) + ' với điểm bmi là: ' + bmi;
+    else if (25.8 < bmi && bmi < 27.3)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(3) + ' với điểm bmi là: ' + bmi;
+    else if (27.3 < bmi && bmi < 32.3)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(4) + ' với điểm bmi là: ' + bmi;
+    else if (32.3 < bmi)
+      return document.querySelector('.ket_qua').innerHTML = 'Bạn: ' + ten + " có tình trạng " + ket_qua(5) + ' với điểm bmi là: ' + bmi;
   }
 
 }
