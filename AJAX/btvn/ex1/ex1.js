746e400a21391b29299a8386a571a8cc31fa3638
khanh.tran <khanh.tran@savvycomsoftware.com>
2022-11-23 19:03:52 +0700

    Update ex1.js

--- AJAX/btvn/ex1/ex1.js
+++ AJAX/btvn/ex1/ex1.js
@@ -1,42 +1,46 @@
 
 var arrUrl = [];
 async function getApi() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-  var data = await response.json();
-  let tab =
-    `<tr></tr>`;
-  for (var i = 0; i < data.results.length; i++) {
-    tab += `
-   <tr>
-    <td>
-       <button type="button" class="list-group-item list-group-item-action">
-         ${data.results[i].name}
-      </button>
-    </td>
-   </tr>`;
-
-    arrUrl.push({
-      "id": data.results[i].name,
-      "val": data.results[i].url,
-    });
+  var tableList = document.getElementById('name');
+  var result = [];
+  
+  try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    
+      if (response.json().length > 0) {
+        result = response.json();
+      }
+  } catch() {
+    return;
   }
-  document.getElementById('name').innerHTML = tab;
+  
+  if (result.length == 0) {
+    var emptyRow = document.createElement('tr');
+    emptyRow.innerHtml = 'KO co du lieu');
+    tableList.appendChild(emptyRow);
+    return;
+  }
+  
+  for (let i = 0; i < result.length; i++){
+    let dataRow = document.createElement('tr');
+    let dataCell = document.creatElement('td');
+    dataCell.innerHtml = results[i].name;
+    
+    dataCell.click = showDetail.bind(this, results[i].name, results[i].url, this);
+    
+    dataRow.appendChild(dataCell);
+    
+    tableList.appendChild(dataRow);
+  }
 }
 getApi();
 
+function showDetail(name, imageLink, event) {
+  document.getElementById('ten_can_sua_doi').innerHTML = name;
+  document.getElementById('anh_sua_doi').src = arrUrl[ull].imageLink;
+  toggleActive(event);
+}
 
-function addActive(event) {
+function toggleActive(event) {
   event.target.classList.toggle('active');
-  var ten = event.srcElement.innerText;
-  document.getElementById('ten_can_sua_doi').innerHTML = ten;
-
-  var ull;
-  for (var i = 0; i < arrUrl.length; i++) {
-    if (ten == arrUrl[i].id) {
-      ull = i + 1;
-    }
-  }
-  document.getElementById('anh_sua_doi').src = arrUrl[ull].val;
-  console.log(document.getElementById('anh_sua_doi').src);
-  
 }
